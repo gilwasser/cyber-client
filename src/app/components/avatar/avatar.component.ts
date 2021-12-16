a6144bdeece34340781a4a4bb0df44d037978cc4
gilwasser <40173646+gilwasser@users.noreply.github.com>
2021-12-16 14:13:46 +0200

    changing strict mode and creating the avatar component

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avatar.component.scss']
 })
 export class AvatarComponent implements OnInit {
-
+  @Input() imgSrc: string;
+  @Input() imgName: string;
   constructor() { }
 
   ngOnInit(): void {
